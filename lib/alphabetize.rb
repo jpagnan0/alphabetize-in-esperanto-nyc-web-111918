e7756c4e31e3825dbe6ebfeb075f4bc28a7a7057
Adam Saland <a.jpagnano@gmail.com>
2018-11-11 17:40:24 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,4 +1,5 @@
 def alphabetize(arr)
   # code here
-    arr.sort_by { |a| a.split("").map{ |char| ESPERANTO_ALPHABET.index(char) } }
+  arr.sort_by { |a| a.split("").map{ |char| ESPERANTO_ALPHABET.index(char) } }
+
 end
